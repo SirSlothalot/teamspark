05187b750563dc454988b0f2ec6562a6e1af6e00
SirSlothalot <matthew.stagoll@gmail.com>
2017-05-18 19:13:56 +0800

    project works

--- teamspark/app_server/controllers/project.js
+++ teamspark/app_server/controllers/project.js
@@ -4,32 +4,39 @@ var Project = mongoose.model('Project');
 var Person = mongoose.model('Person');
 
 module.exports.load = function(req, res, next) {
-      res.render('project-create', { title: 'New Project'});
+    if(req.user) {
+        res.render('project-create', { title: 'New Project', user: req.user});
+    } else {
+        res.redirect('/');
+    }
 };
 
 module.exports.submit = function(req, res, next) {
 
-    var p = Person.findOne({"username":req.user});
+    var p = Person.findOne({"username":req.params.user});
 
-    Project.insert(
-        {
-            title: req.body.title,
-            description: req.body.description,
+    var newProject = new Project({
+        title: req.body.title,
+        description: req.body.description,
 
-            skillLevel: req.body.skillLevel,
-            programmingLanguages: req.body.programmingLanguages,
+        skillLevel: req.body.skillLevel,
+        programmingLanguages: req.body.programmingLanguages,
 
-            timePerWeek: req.body.timePerWeek,
-            online: req.body.online,
+        timePerWeek: req.body.timePerWeek,
+        online: req.body.online,
 
-            country: p.country,
-            state: p.state,
-            suburb: p.suburb,
+        country: p.country,
+        state: p.state,
+        suburb: p.suburb,
 
-            ageOfOwner: p.dob,
-            submissionDate: new Date(),
+        ageOfOwner: p.dob,
+        submissionDate: new Date(),
 
-            mainSpokenLanguage: p.mainSpokenLanguage
-        }
-    );
-};
+        mainSpokenLanguage: p.mainSpokenLanguage
+    });
+
+    newProject.save();
+    res.redirect('/');
+
+
+}
