0046c6f16ba9ff4941531884627cfd5a4dd2b728
fezproof <stevebob1997@hotmail.com>
2017-05-26 00:34:14 +0800

    some layout changes and matching algorithm continued

--- teamspark/app_server/controllers/match.js
+++ teamspark/app_server/controllers/match.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var PQueue = require("fastpriorityqueue");
 var Project = mongoose.model('Project');
 var Person = mongoose.model('Person');
+var matcher = require('./matching');
 
 module.exports.renderAllProjects = function (req,res){
     if(req.user) {
@@ -14,22 +15,8 @@ module.exports.renderAllProjects = function (req,res){
                         error: err
                     });
                 } else {
-                    var projects = [];
-                    var numProjects = result.projectPotentials.length;
-                    for(var i = 0; i < numProjects; i++) {
-
-                        Project.findOne({"title": result.projectPotentials[i]},
-                            function(err, project) {
-                                if(err) {
-                                    console.log("Couldn't find project: " + result.projectPotentials[i]);
-                                } else {
-                                    projects.push(project);
-                                }
-                        });
-
-                    }
-                    console.log('find complete');
-                    res.render('potential-projects', {'user':result, 'projects':projects, 'project':res.app.locals.project});
+                    console.log('find complete1');
+                    matcher.findProjects(result, req, res);
                 }
             })
     } else {
@@ -50,70 +37,11 @@ module.exports.renderAllPeople = function(req, res, next) {
                     });
                 } else {
                     console.log('find complete1');
-                    findPeople(result, req, res);
+                    matcher.findPeople(result, req, res);
                 }
             });
     } else {
         console.log("Cannot view project. You are not logged in.");
         res.redirect('/');
     }
-}
-
-function userNode(user, score) {
-  this.user = user;
-  this.score = score;
-}
-
-function findPeople(project, req, res) {
-    var users = new PQueue(function(a, b) { return a.score > b.score });
-    var query = Person.find().exec(
-        function(err, simpleData) {
-            if(err) {
-                res.render('error', {
-                    message:err.messagr,
-                    error: err
-                });
-            } else {
-                console.log('find complete2');
-                //console.log(simpleData);
-                for (var k = 0; k < simpleData.length; k++) {
-                    var node = comparePerson(project, simpleData[k]);
-                    users.add(node);
-                }
-                var userArray = [];
-                while(!users.isEmpty()) {
-                  var foo = users.poll();
-                  userArray.push(foo.user);
-                }
-                console.log('find complete3');
-                res.render('potential-users', {'people':userArray, 'project':res.app.locals.project, user: req.user});
-            }
-        });
-}
-
-function comparePerson(project, user) {
-  var usernode = new userNode(user, 0);
-  if (!project.virtualTeam) {
-
-  }
-  for(var i = 0; i < project.programmingLanguages.length; i++) {
-    for(var j = 0; j < user.programmingLanguages.length; j++) {
-      if (project.programmingLanguages[i] == user.programmingLanguages[j]) {
-        usernode.score += 4;
-      }
-    }
-  }
-  if (project.skillLevel <= user.skillLevel) {
-    usernode.score += (1 + user.skillLevel - project.skillLevel) * 3;
-  }
-  if (user.availability >= project.workload) {
-    usernode.score += (1 + user.availability - project.workload) * 2;
-  }
-  var userDate = new Date(user.dob);
-  var projectDate = new Date(project.ageOfOwner);
-  var time = Math.floor((userDate - projectDate) / (1000*60*60*24*365))
-  if (time <= 10) {
-    usernode.score += 1 + 10 - time;
-  }
-  return usernode;
 }
--- teamspark/app_server/controllers/matching.js
+++ teamspark/app_server/controllers/matching.js
@@ -3,10 +3,100 @@ var mongoose = require('mongoose');
 var Project = mongoose.model('Project');
 var Person = mongoose.model('Person');
 
-function findProjects() {
-    
+function Node(data, score) {
+  this.data = data;
+  this.score = score;
 }
 
-function findPersons() {
+function findProjects() {
+  var users = new PQueue(function(a, b) {
+    return a.score > b.score
+  });
+  var query = Project.find().exec(
+    function(err, simpleData) {
+      if (err) {
+        res.render('error', {
+          message: err.messagr,
+          error: err
+        });
+      } else {
+        console.log('find complete2');
+        //console.log(simpleData);
+        for (var k = 0; k < simpleData.length; k++) {
+          var node = comparePerson(project, simpleData[k]);
+          users.add(node);
+        }
+        var userArray = [];
+        while (!users.isEmpty()) {
+          var foo = users.poll();
+          userArray.push(foo.data);
+        }
+        console.log('find complete3');
+        res.render('potential-users', {
+          'people': userArray,
+          'project': res.app.locals.project,
+          user: req.user
+        });
+      }
+    });
+}
 
+function findPeople(project, req, res) {
+  var users = new PQueue(function(a, b) {
+    return a.score > b.score
+  });
+  var query = Person.find().exec(
+    function(err, simpleData) {
+      if (err) {
+        res.render('error', {
+          message: err.messagr,
+          error: err
+        });
+      } else {
+        console.log('find complete2');
+        //console.log(simpleData);
+        for (var k = 0; k < simpleData.length; k++) {
+          var node = comparePerson(project, simpleData[k]);
+          users.add(node);
+        }
+        var userArray = [];
+        while (!users.isEmpty()) {
+          var foo = users.poll();
+          userArray.push(foo.data);
+        }
+        console.log('find complete3');
+        res.render('potential-users', {
+          'people': userArray,
+          'project': res.app.locals.project,
+          user: req.user
+        });
+      }
+    });
+}
+
+function comparePerson(project, user) {
+  var usernode = new Node(user, 0);
+  if (!project.virtualTeam) {
+
+  }
+  for (var i = 0; i < project.programmingLanguages.length; i++) {
+    for (var j = 0; j < user.programmingLanguages.length; j++) {
+      if (project.programmingLanguages[i] == user.programmingLanguages[j]) {
+        usernode.score += 4;
+      }
+    }
+  }
+  if (project.skillLevel <= user.skillLevel) {
+    usernode.score += (1 + user.skillLevel - project.skillLevel) * 3;
+  }
+  if (user.availability >= project.workload) {
+    usernode.score += (1 + user.availability - project.workload) * 2;
+  }
+  var userDate = new Date(user.dob);
+  var projectDate = new Date(project.ageOfOwner);
+  var time = Math.floor((userDate - projectDate) / (1000 * 60 * 60 * 24 * 365))
+  if (time <= 10) {
+    usernode.score += 1 + 10 - time;
+  }
+  return usernode;
 }
