44240bdff37e22a4d69921060e21b9bcd8e3eecc
fezproof <stevebob1997@hotmail.com>
2017-05-26 01:10:54 +0800

    matching almost complete

--- teamspark/app_server/controllers/matching.js
+++ teamspark/app_server/controllers/matching.js
@@ -8,8 +8,8 @@ function Node(data, score) {
   this.score = score;
 }
 
-function findProjects() {
-  var users = new PQueue(function(a, b) {
+function findProjects(user, req, res) {
+  var projects = new PQueue(function(a, b) {
     return a.score > b.score
   });
   var query = Project.find().exec(
@@ -23,22 +23,48 @@ function findProjects() {
         console.log('find complete2');
         //console.log(simpleData);
         for (var k = 0; k < simpleData.length; k++) {
-          var node = comparePerson(project, simpleData[k]);
-          users.add(node);
+          var node = compareProject(simpleData[k], user);
+          projects.add(node);
         }
-        var userArray = [];
+        var projectArray = [];
         while (!users.isEmpty()) {
           var foo = users.poll();
-          userArray.push(foo.data);
+          projectArray.push(foo.data);
         }
         console.log('find complete3');
-        res.render('potential-users', {
-          'people': userArray,
-          'project': res.app.locals.project,
+        res.render('potential-projects', {
+          'projects': projectArray,
           user: req.user
         });
       }
     });
+}
+
+function compareProject(project, user) {
+  var usernode = new Node(project, 0);
+  if (!project.virtualTeam) {
+
+  }
+  for (var i = 0; i < project.programmingLanguages.length; i++) {
+    for (var j = 0; j < user.programmingLanguages.length; j++) {
+      if (project.programmingLanguages[i] == user.programmingLanguages[j]) {
+        usernode.score += 4;
+      }
+    }
+  }
+  if (project.skillLevel <= user.skillLevel) {
+    usernode.score += (1 + user.skillLevel - project.skillLevel) * 3;
+  }
+  if (user.availability >= project.workload) {
+    usernode.score += (1 + user.availability - project.workload) * 2;
+  }
+  var userDate = new Date(user.dob);
+  var projectDate = new Date(project.ageOfOwner);
+  var time = Math.floor((userDate - projectDate) / (1000 * 60 * 60 * 24 * 365))
+  if (time <= 10) {
+    usernode.score += 1 + 10 - time;
+  }
+  return usernode;
 }
 
 function findPeople(project, req, res) {
